4187d832471454ecbee15bcc244cd85e3299d205
Novonita <novonitxx.h26@gmail.com>
2026-08-06 10:16:46 +0530

    Time: 12 ms (28.83%), Space: 19.2 MB (86.74%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,19 +1,16 @@
 class Solution:
-    def isPalindrome(self, x: int) -> int:
-        # Negative numbers are never palindromes
-        # Numbers ending in 0 are never palindromes (except 0 itself)
+    def isPalindrome(self, x: int) -> bool:
         if x < 0 or (x % 10 == 0 and x != 0):
             return False
-
-        reverted = 0
-        while x > reverted:
-            reverted = reverted * 10 + x % 10
-            x //= 10
-
-        # For even digit count: x == reverted
-        # For odd digit count: x == reverted // 10 (middle digit doesn't matter)
-        return x == reverted or x == reverted // 10
-
-
+        
+        num = x
+        res = 0
+        while num > 0:
+            ld = num % 10
+            res = (res * 10) + ld
+            num = num // 10
+        
+        return x == res
+        
 
         
